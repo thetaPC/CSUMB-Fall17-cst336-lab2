f01de16c2cb44eb0c633a09ed0e787b57894df32
Maria Loza <mloza@csumb.edu>
2017-09-12 22:14:54 +0000

    Jackpot sound works on Heroku/Github

--- inc/functions.php
+++ inc/functions.php
@@ -2,7 +2,7 @@
 
     function play() {
         for ($i = 1; $i < 4; $i++) {
-            ${"randomValue" . $i } = 0;
+            ${"randomValue" . $i } = rand(0,3);
             displaySymbol(${"randomValue" . $i}, $i);
         }
         displayPoints($randomValue1, $randomValue2, $randomValue3);
